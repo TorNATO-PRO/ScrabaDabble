e87fcc584e2a58cfbd0c22971b58ca4c20e1ff97
TorNATO-PRO <nathan.waltz@wsu.edu>
2020-12-31 10:29:56 -0800

    Stylistic Changes and added more functions of the functional variety to improve readability and professionalism

--- src/TorNATO/dev/Main.java
+++ src/TorNATO/dev/Main.java
@@ -50,251 +50,267 @@ import java.util.stream.IntStream;
  */
 public class Main {
 
-    // fields for storing the maximum possible input, a wordlist, and character values
-    private static final int MAX_INPUT = 10;
-    private static Map<Character, Integer> characterValues;
+  // fields for storing the maximum possible input, a wordlist, and character values
+  private static final int MAX_INPUT = 10;
+  private static Map<Character, Integer> characterValues;
 
-    /**
-     * A dictionary of all the words that are available in scrabble
-     */
-    public static Map<String, Word> wordList;
+  /**
+   * A dictionary of all the words that are available in scrabble
+   */
+  public static Map<String, Word> wordList;
 
-    // loads data from files
-    static {
-        try {
-            characterValues = readCharacterValues("CharacterValues.txt");
-            wordList = readDictionary("EnglishDictionary.txt");
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+  // loads data from files
+  static {
+    try {
+      characterValues = readCharacterValues("CharacterValues.txt");
+      wordList = readDictionary("EnglishDictionary.txt");
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
+  }
 
-    /**
-     * Main method
-     * @param args Unused command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner userIn = new Scanner(System.in);
-        System.out.println("Welcome to my scrabble cheating program! Press (:C) to");
-        System.out.println("cheat and (:L) to lookup. Also, press (:Q) to quit. Enjoy!");
-        for (; ; ) {
-            String firstInput = userIn.nextLine();
-            if (firstInput.equalsIgnoreCase(":C")) {
-                cheat(userIn);
-            } else if (firstInput.equalsIgnoreCase(":L")) {
-                lookup(userIn);
-            } else if (firstInput.equalsIgnoreCase(":Q")) {
-                break;
-            } else {
-                System.out.println("Your input could not be understood!");
-            }
-            System.out.println();
-        }
+  /**
+   * Main method
+   * @param args Unused command line arguments
+   */
+  public static void main(String[] args) {
+    Scanner userIn = new Scanner(System.in);
+    System.out.println(
+      "Welcome to my scrabble cheating program! Press (:C) to"
+    );
+    System.out.println(
+      "cheat and (:L) to lookup. Also, press (:Q) to quit. Enjoy!"
+    );
+    for (;;) {
+      String firstInput = userIn.nextLine();
+      if (firstInput.equalsIgnoreCase(":C")) {
+        cheat(userIn);
+      } else if (firstInput.equalsIgnoreCase(":L")) {
+        lookup(userIn);
+      } else if (firstInput.equalsIgnoreCase(":Q")) {
+        break;
+      } else {
+        System.out.println("Your input could not be understood!");
+      }
+      System.out.println();
     }
+  }
 
-    /**
-     * readDictionary method: Takes in a filename and reads the words quickly
-     * using a BufferedReader, and afterwards stores the words in a
-     * dictionary for O(1) access time
-     *
-     * @param filename A filename which contains the path to the dictionary
-     * @return A HashMap containing a dictionary which consists of the word as a
-     * String and the corresponding Word object
-     * @throws IOException If something goes wrong with reading the file, it throws
-     * this exception
-     */
-    public static Map<String, Word> readDictionary(String filename) throws IOException {
-        BufferedReader br;
-        HashMap<String, Word> wordMap = new HashMap<>();
-        try {
-            br = new BufferedReader(new FileReader(filename));
-            String currentLine;
-            while ((currentLine = br.readLine()) != null) {
-                if (currentLine.length() < 8) {
-                    boolean isAlphabetic = true;
-                    currentLine = currentLine.toUpperCase();
-                    char[] charArray = currentLine.toCharArray();
-                    for (char c : charArray) {
-                        if (!Character.isAlphabetic(c)) {
-                            isAlphabetic = false;
-                            break;
-                        }
-                    }
-                    if (isAlphabetic) {
-                        wordMap.put(currentLine, new Word(currentLine, score(currentLine)));
-                    }
-                }
-            }
-            br.close();
-        } catch (FileNotFoundException exception) {
-            exception.printStackTrace();
-        }
-        return wordMap;
-    }
-
-    /**
-     * readDictionary method: Takes in a filename and reads the characters quickly
-     * using a BufferedReader, and afterwards stores the words in a
-     * dictionary for O(1) access time
-     *
-     * @param filename A filename which contains the path to the dictionary
-     * @return A HashMap containing a dictionary which consists of a character as
-     * well as score of that character
-     * @throws IOException If something goes wrong with reading the file, it throws
-     * this exception
-     */
-    public static Map<Character, Integer> readCharacterValues(String filename)
-            throws IOException {
-        BufferedReader br;
-        Map<Character, Integer> valueOfCharacter = new HashMap<>();
-        try {
-            br = new BufferedReader(new FileReader(filename));
-            String currentLine;
-            while ((currentLine = br.readLine()) != null) {
-                String[] values = currentLine.split(",");
-                Character character = values[0].trim().charAt(0);
-                Integer val = Integer.parseInt(values[1].trim());
-                valueOfCharacter.put(character, val);
-            }
-            br.close();
-        } catch (FileNotFoundException exception) {
-            exception.printStackTrace();
-        }
-        return valueOfCharacter;
-    }
-
-    /**
-     * Cheat method: Allows the user to cheat given a sequence
-     * of characters
-     *
-     * @param userIn A scanner parameter which allows
-     *               for the user to enter a character sequence
-     */
-    public static void cheat(Scanner userIn) {
-        System.out.println("Please enter a character sequence");
-        String input = userIn.nextLine().toUpperCase();
-        if (!isAlphabetic(input) || input.length() > MAX_INPUT) {
-            System.out.println("That is not a valid input!");
-        } else {
-            Set<Word> powerSet = genSubsets(input.toCharArray());
-            for (Word word : powerSet) {
-                System.out.println("Word: " + word.getWord() + ", Score: " + word.getScore());
-            }
-        }
-    }
-
-    /**
-     * Lookup method: Allows the user to see if a given String
-     * exists
-     *
-     * @param userIn A scanner parameter which allows
-     *               for the user to enter a word
-     */
-    public static void lookup(Scanner userIn) {
-        System.out.println("Please enter a word");
-        String input = userIn.nextLine();
-        if (wordList.containsKey(input.toUpperCase())) {
-            System.out.println(input + " is a valid word with a score of "
-                    + wordList.get(input).getScore() + "!");
-        } else {
-            System.out.println("Invalid word!");
-        }
-    }
-
-    /**
-     * Checks to see if all of the character in a String
-     * are alphabetic
-     *
-     * @param input A given String provided by the client
-     * @return Boolean representing whether or not all of the
-     * characters in the input are alphabetic
-     */
-    private static boolean isAlphabetic(String input) {
-        char[] charArray = input.toCharArray();
-        for (char c : charArray) {
+  /**
+   * readDictionary method: Takes in a filename and reads the words quickly
+   * using a BufferedReader, and afterwards stores the words in a
+   * dictionary for O(1) access time
+   *
+   * @param filename A filename which contains the path to the dictionary
+   * @return A HashMap containing a dictionary which consists of the word as a
+   * String and the corresponding Word object
+   * @throws IOException If something goes wrong with reading the file, it throws
+   * this exception
+   */
+  public static Map<String, Word> readDictionary(String filename)
+    throws IOException {
+    BufferedReader br;
+    HashMap<String, Word> wordMap = new HashMap<>();
+    try {
+      br = new BufferedReader(new FileReader(filename));
+      String currentLine;
+      while ((currentLine = br.readLine()) != null) {
+        if (currentLine.length() < 8) {
+          boolean isAlphabetic = true;
+          currentLine = currentLine.toUpperCase();
+          char[] charArray = currentLine.toCharArray();
+          for (char c : charArray) {
             if (!Character.isAlphabetic(c)) {
-                return false;
+              isAlphabetic = false;
+              break;
             }
+          }
+          if (isAlphabetic) {
+            wordMap.put(currentLine, new Word(currentLine, score(currentLine)));
+          }
         }
-        return true;
+      }
+      br.close();
+    } catch (FileNotFoundException exception) {
+      exception.printStackTrace();
+    }
+    return wordMap;
+  }
+
+  /**
+   * readDictionary method: Takes in a filename and reads the characters quickly
+   * using a BufferedReader, and afterwards stores the words in a
+   * dictionary for O(1) access time
+   *
+   * @param filename A filename which contains the path to the dictionary
+   * @return A HashMap containing a dictionary which consists of a character as
+   * well as score of that character
+   * @throws IOException If something goes wrong with reading the file, it throws
+   * this exception
+   */
+  public static Map<Character, Integer> readCharacterValues(String filename)
+    throws IOException {
+    BufferedReader br;
+    Map<Character, Integer> valueOfCharacter = new HashMap<>();
+    try {
+      br = new BufferedReader(new FileReader(filename));
+      String currentLine;
+      while ((currentLine = br.readLine()) != null) {
+        String[] values = currentLine.split(",");
+        Character character = values[0].trim().charAt(0);
+        Integer val = Integer.parseInt(values[1].trim());
+        valueOfCharacter.put(character, val);
+      }
+      br.close();
+    } catch (FileNotFoundException exception) {
+      exception.printStackTrace();
+    }
+    return valueOfCharacter;
+  }
+
+  /**
+   * Cheat method: Allows the user to cheat given a sequence
+   * of characters
+   *
+   * @param userIn A scanner parameter which allows
+   *               for the user to enter a character sequence
+   */
+  public static void cheat(Scanner userIn) {
+    System.out.println("Please enter a character sequence");
+    String input = userIn.nextLine().toUpperCase();
+    if (!isAlphabetic(input) || input.length() > MAX_INPUT) {
+      System.out.println("That is not a valid input!");
+    } else {
+      Set<Word> powerSet = genSubsets(input.toCharArray());
+      for (Word word : powerSet) {
+        System.out.println(
+          "Word: " + word.getWord() + ", Score: " + word.getScore()
+        );
+      }
+    }
+  }
+
+  /**
+   * Lookup method: Allows the user to see if a given String
+   * exists
+   *
+   * @param userIn A scanner parameter which allows
+   *               for the user to enter a word
+   */
+  public static void lookup(Scanner userIn) {
+    System.out.println("Please enter a word");
+    String input = userIn.nextLine();
+    String upperCaseInput = input.toUpperCase();
+    if (wordList.containsKey(upperCaseInput)) {
+      System.out.println(
+        input +
+        " is a valid word with a score of " +
+        wordList.get(upperCaseInput).getScore() +
+        "!"
+      );
+    } else {
+      System.out.println("Invalid word!");
+    }
+  }
+
+  /**
+   * Checks to see if all of the character in a String
+   * are alphabetic
+   *
+   * @param input A given String provided by the client
+   * @return Boolean representing whether or not all of the
+   * characters in the input are alphabetic
+   */
+  private static boolean isAlphabetic(String input) {
+    char[] charArray = input.toCharArray();
+    for (char c : charArray) {
+      if (!Character.isAlphabetic(c)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Generates a "power set" of characters from a given character
+   * array
+   *
+   * @param word A character array which contains characters for which
+   *             we wish to find a subset
+   * @return A "power set" of all the characters that are
+   * contained in the passed word character array
+   */
+  public static Set<Word> genSubsets(char[] word) {
+    Set<Word> subsets = new TreeSet<>();
+    if (word.length == 0) {
+      return subsets;
+    }
+    genSubsets(word, subsets, new Stack<>(), 0);
+    return subsets;
+  }
+
+  /**
+   * A helper method which does the heavy lifting and generates the subsets
+   * for the power set
+   *
+   * @param word A character array which contains characters for which
+   *             we wish to find a subset
+   * @param subsets A set of subsets of the word parameter
+   * @param temp A stack of characters which stores values obtained throughout
+   *             the execution of this method
+   * @param index The current index
+   */
+  private static void genSubsets(
+    char[] word,
+    Set<Word> subsets,
+    Stack<Character> temp,
+    int index
+  ) {
+    String result = toString(new ArrayList<>(temp));
+    if (wordList.containsKey(result)) {
+      subsets.add(wordList.get(result));
     }
 
-    /**
-     * Generates a "power set" of characters from a given character
-     * array
-     *
-     * @param word A character array which contains characters for which
-     *             we wish to find a subset
-     * @return A "power set" of all the characters that are
-     * contained in the passed word character array
-     */
-    public static Set<Word> genSubsets(char[] word) {
-        Set<Word> subsets = new TreeSet<>();
-        if (word.length == 0) {
-            return subsets;
+    // loops through the word and recursively backtracks
+    // in a functional paradigm manner
+    IntStream
+      .range(index, word.length)
+      .forEach(
+        i -> {
+          temp.push(word[i]);
+          genSubsets(word, subsets, temp, i + 1);
+          temp.pop();
         }
-        genSubsets(word, subsets, new Stack<>(), 0);
-        return subsets;
+      );
+  }
+
+  /**
+   * Converts a list of characters to a String
+   *
+   * @param array A list of characters which will be converted to
+   *              a string
+   * @return A string representation of the passed array parameter
+   */
+  private static String toString(List<Character> array) {
+    if (array.size() == 0) {
+      return "";
     }
+    StringBuilder temp = new StringBuilder();
+    array.forEach(temp::append);
+    return temp.toString();
+  }
 
-    /**
-     * A helper method which does the heavy lifting and generates the subsets
-     * for the power set
-     *
-     * @param word A character array which contains characters for which
-     *             we wish to find a subset
-     * @param subsets A set of subsets of the word parameter
-     * @param temp A stack of characters which stores values obtained throughout
-     *             the execution of this method
-     * @param index The current index
-     */
-    private static void genSubsets(char[] word, Set<Word> subsets,
-                                   Stack<Character> temp, int index) {
-        String result = toString(new ArrayList<>(temp));
-        if (wordList.containsKey(result)) {
-            subsets.add(wordList.get(result));
-        }
-
-        // loops through the word and recursively backtracks
-        // in a functional paradigm manner
-        IntStream.range(index, word.length).forEach(i -> {
-            temp.push(word[i]);
-            genSubsets(word, subsets, temp, i + 1);
-            temp.pop();
-        });
-    }
-
-    /**
-     * Converts a list of characters to a String
-     *
-     * @param array A list of characters which will be converted to
-     *              a string
-     * @return A string representation of the passed array parameter
-     */
-    private static String toString(List<Character> array) {
-        if (array.size() == 0) {
-            return "";
-        }
-        StringBuilder temp = new StringBuilder();
-        for (Character c : array) {
-            temp.append(c);
-        }
-        return temp.toString();
-    }
-
-    /**
-     * Finds a score for a given word
-     *
-     * @param word A word passed by the client
-     * @return A score corresponding to the cumulative
-     * score of that word
-     */
-    public static int score(String word) {
-        int score = 0;
-        for (char c : word.toCharArray()) {
-            score += characterValues.get(c);
-        }
-        return score;
-    }
-
+  /**
+   * Finds a score for a given word
+   *
+   * @param word A word passed by the client
+   * @return A score corresponding to the cumulative
+   * score of that word
+   */
+  public static int score(String word) {
+    return IntStream
+      .range(0, word.length())
+      .map(i -> characterValues.get(word.charAt(i)))
+      .sum();
+  }
 }
--- src/TorNATO/dev/Word.java
+++ src/TorNATO/dev/Word.java
@@ -42,52 +42,52 @@ package TorNATO.dev;
  */
 public class Word implements Comparable<Word> {
 
-    // fields storing the String representation
-    // of the word, and the score
-    private final String word;
-    private final int score;
+  // fields storing the String representation
+  // of the word, and the score
+  private final String word;
+  private final int score;
 
-    /**
-     * Constructor for the Word object
-     *
-     * @param word The word which is inputted by
-     *             the client as a String
-     * @param score The score which corresponds to
-     *              to the cumulative sum of all the character's scores
-     *              in the word
-     */
-    public Word(String word, int score) {
-        this.word = word;
-        this.score = score;
-    }
+  /**
+   * Constructor for the Word object
+   *
+   * @param word The word which is inputted by
+   *             the client as a String
+   * @param score The score which corresponds to
+   *              to the cumulative sum of all the character's scores
+   *              in the word
+   */
+  public Word(String word, int score) {
+    this.word = word;
+    this.score = score;
+  }
 
-    /**
-     * compareTo: Compares one word to another
-     *
-     * @param other Another Word object that we want to compare
-     * @return An integer corresponding to the comparison between
-     * the values of these two objects, namely their scores
-     */
-    @Override
-    public int compareTo(Word other) {
-        return other.score - this.score;
-    }
+  /**
+   * compareTo: Compares one word to another
+   *
+   * @param other Another Word object that we want to compare
+   * @return An integer corresponding to the comparison between
+   * the values of these two objects, namely their scores
+   */
+  @Override
+  public int compareTo(Word other) {
+    return other.score - this.score;
+  }
 
-    /**
-     * getWord: Getter for the word
-     *
-     * @return A string representation of the word
-     */
-    public String getWord() {
-        return word;
-    }
+  /**
+   * getWord: Getter for the word
+   *
+   * @return A string representation of the word
+   */
+  public String getWord() {
+    return word;
+  }
 
-    /**
-     * getScore: Getter for the score
-     *
-     * @return An integer representation of the score
-     */
-    public int getScore() {
-        return score;
-    }
+  /**
+   * getScore: Getter for the score
+   *
+   * @return An integer representation of the score
+   */
+  public int getScore() {
+    return score;
+  }
 }
